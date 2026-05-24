f7cc066f6aa4059e59b6299dbddfbde0764937eb
rickey <rikun2894@gmail.com>
2026-05-24 14:20:00 +0900

    session整理

--- server.js
+++ server.js
@@ -44,13 +44,6 @@ db.connect(err => {
 // =======================
 app.set("trust proxy", 1);
 
-const sessionStore = new MySQLStore({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
